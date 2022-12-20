65b2619faa076cecaa9ca14c88e81ee027cf0095
oluwafemimicheal <oluwafemicu@gmail.com>
2022-12-20 03:50:39 +0100

    master

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -1,24 +1,33 @@
-import {Link, Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './Component/Home/Home.js'
 import About from './Component/About/About.jsx'
 import Portfolio from './Component/Portfolio/Portfolio.jsx'
 import Footer from './Component/Footer/Footer.jsx'
 import Error from './Component/Error/Error.jsx'
 
-const App =()=>{
-    return(
-      <div>
-        <ul>
-            <li><Link to='/'>React</Link></li>
-        </ul>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/footer' element={<Footer/>} />
-            <Route path='/portfolio' element={<Portfolio/>}/>
-            <Route path='/*' element={<Error/>}/>
-        </Routes>
-      </div>
+const App = () => {
+    return ( <
+        div >
+        <
+        Routes >
+        <
+        Route path = '/'
+        element = { < Home / > }
+        /> <
+        Route path = '/about'
+        element = { < About / > }
+        /> <
+        Route path = '/footer'
+        element = { < Footer / > }
+        /> <
+        Route path = '/portfolio'
+        element = { < Portfolio / > }
+        /> <
+        Route path = '/*'
+        element = { < Error / > }
+        /> <
+        /Routes> <
+        /div>
     )
 }
 
--- Portfolio/src/Component/Error/Error.jsx
+++ Portfolio/src/Component/Error/Error.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import {Link} from "react-router-dom";
+import Robot from './images/Robot.jpg'
+import './error.css'
 
 const Error = ()=>{
     return(
         <>
-        <h1>Page Not Found</h1>   
+        <div className="error-container">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <img src={Robot} alt="Loading" />
+            <p>This page is not part of the HTTP server; please return to the home page for more important information on this website, or perhaps there is a word missing from the link entered in the URL box.!</p>
+            <Link to='/'>Back to Home!</Link>
+        </div>  
         </>
     )
 }
