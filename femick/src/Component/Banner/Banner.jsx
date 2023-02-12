33de3346eee83222787ef01d256f89fbc60f6789
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-12 23:55:41 +0100

    master

--- femick/src/Component/Banner/Banner.jsx
+++ femick/src/Component/Banner/Banner.jsx
@@ -6,9 +6,13 @@ import './banner.css'
         <div className="b-container">
             <div className="banner">
                 <div className="text">
+                    <div>
                     <h1>Femick Graphics Design</h1>
                     <p>Creative in all Design and UI & UX</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum atque perferendis nesciunt beatae eaque nihil? Id, quidem quis officia velit laboriosam, cumque non quae sequi reprehenderit in itaque reiciendis sunt!</p>
+                    </div>
+                    <div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum atque perferendis nesciunt beatae eaque nihil? Id, quidem quis officia velit laboriosam, cumque non quae sequi reprehenderit in itaque reiciends sunt!</p>
+                    </div>
                     <button className="btn">Learn More!</button>
                 </div>
                 <div className="image">
