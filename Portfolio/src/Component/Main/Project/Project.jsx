989ab59496e5695cf8c533c999f010bb600dce9a
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-14 09:41:23 +0100

    master

--- Portfolio/src/Component/Main/Project/Project.jsx
+++ Portfolio/src/Component/Main/Project/Project.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import './Project.css'
-import pImage1 from '../image/social-market.png'
+import Catering from '../image/Catering Website.png'
+import Attendance from '../image/Attendance.png'
+import UrlPicker from '../image/Url Picker.png'
+import Message from '../massage/message';
 
 const Project = ()=>{ 
+    const[ megPop, setMegPop] = useState(false)
     return(
         <div className="project" id='Project'>
             <div className="p-left">
@@ -13,15 +17,22 @@ const Project = ()=>{
             </div>
             <div className="p-right">
                 <div className="p-card">
-                    <ProjectCard image={pImage1} Title='Work Attendance Book' text={"The work attendance book is the place where employees sign in and out for the daily work completed."}/>
+                    <ProjectCard image={Catering} Title='Catering Website' text={"The work attendance book is the place where employees sign in and out for the daily work completed."}/>
                 </div>
                 <div className="p-card">
-                    <ProjectCard image={pImage1} Title='Friend Card' text={'Friend Cards enable you to make personal notes about your friends, keep their information in the cloud, and select the type of friend they fall under.'}/>
+                    <ProjectCard image={Attendance} Title='Attendance Checkbook' text={'Friend Cards enable you to make personal notes about your friends, keep their information in the cloud, and select the type of friend they fall under.'}/>
                 </div>
                 <div className="p-card">
-                    <ProjectCard image={pImage1} Title='Bookstore' text={'This allows you to save all of your favorite books that you want to read online in your own category store on the Did App!'}/>
+                    <ProjectCard image={UrlPicker} Title='Chrome Extension/Url-Picker' text={'This allows you to save all of your favorite books that you want to read online in your own category store on the Did App!'}/>
                 </div>
             </div>
+            <Message trigger={megPop}>
+                <FaWhatsapp style={{fontSize: 50, color: 'green'}}/>
+                <button className='remove' onClick={()=> setMegPop(false)}><FaTimes/></button>
+                <p>Hy, Friend let start chatting!</p>
+                <h1>09163657267</h1>
+                <button className='button'>Let's Chat</button>
+            </Message>
         </div>
     )
 }
