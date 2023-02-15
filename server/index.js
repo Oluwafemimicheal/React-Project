147453c33be95a261ed1d690dc590c16d707c248
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-15 03:13:31 +0100

    master

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
 const app = express()
-dotenv().config()
+dotenv.config()
 
-app.get((req, res) => {
+
+app.listen(process.env.port, (req, res) => {
     res.send(console.log('server is working'))
 })
