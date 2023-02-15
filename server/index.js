befd231d4f848a40b3dfeeddfd68156dedae2860
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-15 03:50:49 +0100

    master

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
+import task from './routes/tasks'
 
 const app = express()
 dotenv.config()
@@ -8,5 +9,7 @@ dotenv.config()
 app.get('/', (req, res) => {
     res.send('Task manager')
 })
+
+app.use('api/vi/tasks', tasks)
 
 app.listen(process.env.port, console.log('Service listing'))
