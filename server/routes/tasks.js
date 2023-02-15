f8589ab460f6ae5063e974cfff3372b3d5a06193
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-15 20:26:16 +0100

    master

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 
-const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/task')
+const {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/task')
 
 
 
