64aedac8d3a236fc896063febf29409a1d075df3
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-15 19:44:06 +0100

    master

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -7,5 +7,5 @@ const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('..
 
 
 router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).(deleteTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 module.exports = router;
