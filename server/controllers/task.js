af3ded007aa28e46330bce469518dbe5206c1d70
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-15 19:40:55 +0100

    master

---
+++ server/controllers/task.js
@@ -0,0 +1,24 @@
+const getAllTasks = (req, res) => {
+    res.send('all items')
+}
+
+const createTask = (req, res) => {
+    res.send('Create Tasks')
+}
+const getTask = (req, res) => {
+    res.send('get single task')
+}
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+const deleteTask = (req, res) => {
+    res.send('delete Tasks')
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.route('/').get((req, res) => {
-    res.send('All Items')
-})
 
+const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/task')
+
+
+
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).(deleteTask)
 module.exports = router;
