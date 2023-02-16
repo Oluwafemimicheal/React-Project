1de64cb0f97318f0121e9a2159667879d0e4b22f
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-16 13:00:15 +0100

    master

--- femick/src/App.jsx
+++ femick/src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import Banner from './Component/Banner/Banner'
-import About from './Component/About/About'
-import Service from './Component/Service/Service'
-import Navbar from './Component/Navbar/Navbar'
-
+import Navbar from './component/Navbar'
 function App() {
   return (
    <>
    <Navbar/>
-   <Banner/>
-   <About/>
-   <Service/>
+   
    </>
   )
 }
