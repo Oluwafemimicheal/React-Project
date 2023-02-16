9c3bffd292f4dcb512d784660023c1dc0b7fb96a
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-16 05:40:56 +0100

    master

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 app.get('/', (req, res) => {
     res.send('Task Manager')
