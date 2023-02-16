374510a970ffb3793af3d9afe8a78015ab355fe0
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-16 13:02:45 +0100

    master

--- femick/src/Component/Navbar/Navbar.jsx
+++ femick/src/Component/Navbar/Navbar.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import './navbar.css'
+import React, {useState, useRef} from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
 import {Link} from 'react-scroll'
+import './Navbar.css'
+
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const navRef = useRef()
+
+    const changeNav = ()=>{
+        if(window.scrollY >= 100){
+            setNav(true)
+        }else{
+            setNav(false)
+        }
+    }
+    window.addEventListener('scroll', changeNav)
+
+    const showNav = ()=>{
+        navRef.current.classList.toggle('responsive-bar')
+       
+    } 
     return (
-        <div className='h-container'>
-            <div className='header'>
-                <div className="logo">
-                    <span></span>
-                    <h1>FD</h1>
-                </div>
-                <nav>
-                <Link to='Home' smooth='smooth'>Home</Link>
-                <Link to='About' smooth='smooth'>About</Link>
-                <Link to='Service' smooth='smooth'>Service</Link>
-                <Link to='Blog' smooth='smooth'>Blog</Link>
-                <Link to='Contact' smooth='smooth'>Contact</Link>
-                </nav>
-                <div className="btn">
-                    <button type='button'>Log In</button>
-                    <button type='button'>Sign Up</button>  
+       <nav className={nav ? 'Navbar-active' : 'Navbar-container'}>
+            <div className= 'container'>
+                <div className="row">
+                    <div className="logo">
+                            <span>OM</span>
+                    </div>
+                    <nav ref={navRef}>
+                    <ul className= 'navbar'>
+                        <li><Link to='Home'spy={true} activeClass='activeClass' smooth={true} duration={1000} onClick={showNav}>Home</Link></li>
+                        <li><Link to='About' spy={true}  smooth={true} duration={1000} onClick={showNav}>About</Link></li>
+                        <li><Link to='Portfolio' spy={true}  smooth={true} duration={1000} onClick={showNav}>Skills</Link></li>
+                        <li><Link to='Blog' spy={true}  smooth={true} duration={1000} onClick={showNav}>Service</Link></li>
+                        <li><Link to='Shop' spy={true}  smooth={true} duration={1000} onClick={showNav}>Project</Link></li>
+                        <li><Link to='Contact' spy={true}  smooth={true} duration={1000} onClick={showNav}>Contact</Link></li>
+                    </ul>
+                    <button onClick={showNav} className='nav-btn nav-close-btn'>
+                        <FaTimes style={{fontSize: 20, color:'steelblue'}}/>
+                    </button>
+                    </nav>
+                    <button onClick={showNav} className='nav-btn nav-bar'>
+                        <FaBars style={{fontSize: 20, color:'steelblue'}}/>
+                    </button>
                 </div>
             </div>
-        </div>
+       </nav>
     );
 }
 
