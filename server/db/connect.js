c1d98f5ae2cdfb3f92f8fe1c058c5f7d218c0139
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-16 05:08:02 +0100

    master

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,4 +2,5 @@
 
   const connectionString = 'mongodb+srv://oluwafemi:young4410@cluster0.kau0vbk.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
+  mongoose.set('strictQuery', false)
   mongoose.connect(connectionString).then(() => console.log('CONNECTED TO THE DB...')).catch((err) => console.log(err))
