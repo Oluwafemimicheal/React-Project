dc6fdde17e56a92a3eb81efb6341dbf3fcc2a036
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-27 16:33:34 +0100

    master

--- Portfolio/src/Component/Contact/Contact.jsx
+++ Portfolio/src/Component/Contact/Contact.jsx
@@ -3,19 +3,21 @@ import './Contact.css'
 const Contact = () => {
 
     return (
-        <section className='container contact' >
-            <div className="header"> 
-                <h1>Get in touch</h1>
-                <p>Although I'm currently looking for a opportunities, my inbox is always open. 
-                if you have any for me or just want to say hi, I'll try my best to get back to you!</p>
-            </div>
-            <div className='form'>
-                <form>
-                        <input type="text"  name='user_name' placeholder=' Enter your Full name' required/>
-                        <input type="email" name='user_email' placeholder='Enter your Email' required />
-                    <textarea name='message' cols="30" rows="10"></textarea>
-                 <button type="submit" value="Send" className='btn' >Send</button>
-                </form>
+        <section className='container'>
+            <div className="contact_container">
+                <div className='contact_form'>
+                    <form>
+                            <input type="text"  name='user_name' placeholder=' Enter your Full name' required/>
+                            <input type="email" name='user_email' placeholder='Enter your Email' required />
+                        <textarea name='message' cols="30" rows="10"></textarea>
+                        <button type="submit" value="Send" className='btn' >Send</button>
+                    </form>
+                </div>
+                <div className="header"> 
+                    <h1>Get in touch</h1>
+                    <p>Although I'm currently looking for a opportunities, my inbox is always open. 
+                    if you have any for me or just want to say hi, I'll try my best to get back to you!</p>
+                </div>
             </div>
         </section>
     );
