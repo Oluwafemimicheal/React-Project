86310e0ca00ed71c760895f8e08f48372f3be155
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-27 15:45:38 +0100

    master

--- Portfolio/src/Component/Main/Project/ProjectCard.jsx
+++ Portfolio/src/Component/Main/Project/ProjectCard.jsx
@@ -3,17 +3,17 @@ import './Project.css'
 
 const ProjectCard = ({image,Title,text,Github,Demo}) => {
     return (
-        <div className='projectCard'>
+        <div className='project_card'>
             <div className="p-image">
-            <img src={image} alt="Loading..." style={{width: '80%'}}/>
+            <img src={image} alt="Loading..." style={{width: '100%'}}/>
             </div>
             <div className="p-detail">
-            <h3>{Title}</h3>
-            <p>{text}</p>
-            </div>
-            <div className="btn">
-              <a href={Github}>Github</a>
-              <a href={Demo}>Demo</a>
+                <h3>{Title}</h3>
+                <p>{text}</p>
+                <div className="btn_container">
+                    <a href={Github} className='btn_primary'>Github</a>
+                    <a href={Demo} className='btn_primary'>Demo</a>
+                </div>
             </div>
         </div>
     );
