a5a1b2567449d2338df7495493f46b8a662ddae2
oluwafemimicheal <oluwafemicu@gmail.com>
2023-02-27 11:54:35 +0100

    master

--- Portfolio/src/Component/Main/Service/Service.jsx
+++ Portfolio/src/Component/Main/Service/Service.jsx
@@ -10,12 +10,6 @@ const Service = () =>{
     return(
         <section className="container" id='Service'>
             <div className="service_container">
-                <div className="service_header">
-                    <h1>My Awesome</h1>
-                    <h2>Service</h2>
-                    <p>These are some of the services I provide to clients worldwide, including both office work and online work.</p>
-                    <a href={Resume} className="btn_primary">Download CV</a>
-                </div>
                 <div className="service_cards">
                     <Card 
                     image={Graphics} 
@@ -25,11 +19,17 @@ const Service = () =>{
                     image={Code} 
                     text1={'FRONTEND DEV'} 
                     text2={'Html, Css 3, Javascript, React.js, Bootstrap 5'}/>
-                    <div></div>
                     <Card 
                     image={Social} 
                     text1={'SOCIAL-MEDIA ADS'} 
                     text2={'Facebook,Instagram, WhatApp, Youtube'}/>
+                    <div></div>
+                </div>
+                <div className="service_header">
+                    <h1>My Awesome</h1>
+                    <h2>Service</h2>
+                    <p>These are some of the services I provide to clients worldwide, including both office work and online work.</p>
+                    <a href={Resume} className="btn_primary">Download CV</a>
                 </div>
             </div>
         </section>
