d4a34cf8702950347dfacaeca8b0cf450c7ffb1f
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-01 16:42:39 +0100

    master

---
+++ femick/src/component/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = () =>{
+    return(
+        <section>
+            <div className="section1"></div>
+            <div className="section2"></div>
+        </section>
+    )
+}
+export default Layout;
