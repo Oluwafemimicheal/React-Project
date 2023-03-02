74459c01ed135c9813de76525f0d7e732629d4fc
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-02 10:38:06 +0100

    master

--- femick/src/component/Layout.jsx
+++ femick/src/component/Layout.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import './Layout.css'
 
 const Layout = () =>{
     return(
-        <section>
-            <div className="section1"></div>
-            <div className="section2"></div>
+        <section className="container">
+            <div className="layout_container">
+                <article>
+                    <h1>This is the Header part for you</h1>
+                    <h3>what you think about the business?</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, facere! Numquam, enim eveniet amet omnis explicabo voluptates atque. Labore explicabo aspernatur consequuntur corrupti. Rem, laudantium impedit ullam cupiditate distinctio labore!</p>
+                    <button>Click Here!</button>
+                </article>
+                <picture>
+                    <span>Image Text</span>
+                    <img src="" alt="" />
+                </picture>
+            </div>
         </section>
     )
 }
