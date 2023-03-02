056ebeab29997f666dc0cc49a9ea52904e743322
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-02 15:30:19 +0100

    master

--- Portfolio/src/Component/Navbar/Navbar.jsx
+++ Portfolio/src/Component/Navbar/Navbar.jsx
@@ -21,6 +21,8 @@ const Navbar = () => {
         navRef.current.classList.toggle('menu-bar')
        
     } 
+
+
     
     return (
        <nav className={nav ? 'Navbar-active' : 'Navbar-container'}>
@@ -28,7 +30,7 @@ const Navbar = () => {
                 <div className="nav-row">
                     <nav ref={navRef}>
                         <ul className= 'navbar'>
-                            <li><Link to='Home'spy={true} smooth={true} duration={1000} onClick={showNav}><FaHome/></Link></li>
+                            <li><Link to='Home'spy={true} smooth={true} duration={1000} onClick={showNav}><FaHome style={{Size= 40}}/></Link></li>
                             <li><Link to='About' spy={true}  smooth={true} duration={1000} onClick={showNav}><FaUser/></Link></li>
                             <li><Link to='Skill' spy={true}  smooth={true} duration={1000} onClick={showNav}><FaSkiing/></Link></li>
                             <li><Link to='Service' spy={true}  smooth={true} duration={1000} onClick={showNav}><FaServer/></Link></li>
