5cd6fde2e6497693255dfbb8061ca4a5335d6b13
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-10 01:55:16 +0100

    master

--- femick/src/component/Platform/cards.jsx
+++ femick/src/component/Platform/cards.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
  export const Cards = ({text, paragraph, icon}) => {
     return (
-        <div className='card container'>
+        <div className='card'>
              <h3>{text}</h3>
                     <p>{paragraph}</p>
                     <span>
                         <i>{icon}</i>
                         <div className="image"></div>
                     </span>
-                    <a  className='btn-primary' href="home">READ MORE</a>
+                    <a  className='btn btn-link' href="home">READ MORE</a>
         </div>
     );
 }
