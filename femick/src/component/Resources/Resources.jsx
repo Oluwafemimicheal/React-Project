25a61f6ae09268e5859b16a968996f17bfd94147
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-10 09:27:49 +0100

    master

--- femick/src/component/Resources/Resources.jsx
+++ femick/src/component/Resources/Resources.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import {useState} from 'react';
+import './Resources.css'
 
 const Resources = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
-        <div>
-            <h1>Resources Page is Ready!</h1>
-        </div>
+        <section>
+            <div className="container resources">
+                <div className="resources-header">
+                    <nav className='resources-navbar'>
+                        <h2>How Our Solution <br/>
+                        Can Help You</h2>
+                        <ul className='link'>
+                            <a href="#" onclick={() => setActiveNav('#')}  className={activeNav === '#' ? 'active' : ' '} >Retail</a>
+                            <a href="#about" onclick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ' '} >Travel</a>
+                            <a href="#platform" onclick={() => setActiveNav('#home')}  className={activeNav === '#about' ? 'active' : ' '} >Payment</a>
+                            <a href="#contact" onclick={() => setActiveNav('#home')}  className={activeNav === '#about' ? 'active' : ' '} >Insurance</a>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </section>
     );
 }
 
--- femick/src/component/Solution/Solution.jsx
+++ femick/src/component/Solution/Solution.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Solution.css'
 
 const Solution = () => {
     return (
-        <div>
-            <h1>Solution Page is Ready!</h1>
-        </div>
+        <section className="solution">
+           <div className="solution-container">
+                <div className="header container">
+                    <h2>Your Data. Your Business</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa quos ex accusamus suscipit exercitationem, atque vitae deleniti natus molestiae optio officiis dolor omnis in alias beatae! Minima obcaecati </p>
+                </div>
+           </div>
+            <div className="video-container">
+                <div className="video-background">
+                    <div className="image"></div>
+                </div>
+            </div>
+        </section>
     );
 }
 
