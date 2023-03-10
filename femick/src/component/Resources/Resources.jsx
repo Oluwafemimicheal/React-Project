d8814e53204cb0bf3b238e5625289564731efb9e
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-10 09:45:02 +0100

    master

--- femick/src/component/Resources/Resources.jsx
+++ femick/src/component/Resources/Resources.jsx
@@ -12,11 +12,14 @@ const Resources = () => {
                         Can Help You</h2>
                         <ul className='link'>
                             <a href="#" onclick={() => setActiveNav('#')}  className={activeNav === '#' ? 'active' : ' '} >Retail</a>
-                            <a href="#about" onclick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ' '} >Travel</a>
-                            <a href="#platform" onclick={() => setActiveNav('#home')}  className={activeNav === '#about' ? 'active' : ' '} >Payment</a>
-                            <a href="#contact" onclick={() => setActiveNav('#home')}  className={activeNav === '#about' ? 'active' : ' '} >Insurance</a>
+                            <a href="#about" onclick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ''}>Travel</a>
+                            <a href="#platform" onclick={() => setActiveNav('#home')}  className={activeNav === '#platform' ? 'active' : ''} >Payment</a>
+                            <a href="#contact" onclick={() => setActiveNav('#home')}  className={activeNav === '#contact' ? 'active' : ''} >Insurance</a>
                         </ul>
                     </nav>
+                    <div className="resources-cards">
+                        
+                    </div>
                 </div>
             </div>
         </section>
---
+++ femick/src/component/Resources/ResourcesCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ResourcesCard = () => {
+    return (
+        <div className='resources-card'>
+            
+        </div>
+    );
+}
+
