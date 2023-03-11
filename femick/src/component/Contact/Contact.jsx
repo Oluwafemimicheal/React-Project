197284148bb6ca4608b72a663cfaa3183f291195
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-11 17:52:43 +0100

    master

--- femick/src/component/Contact/Contact.jsx
+++ femick/src/component/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact page is ready!</h1>
+       <section>
+        <div className="container contact-container">
+            <article>
+                <h2>How to Choose<br/> a Femicklization Solution</h2>
+                <small>Make sure you're asking the right question by<br/> reading our free ebook now.</small>
+                <div className='buttons'>
+                    <a className='btn btn-primary' href="">REQUEST A DEMO</a>
+                    <a className='btn btn-black'  href="">CONTACT US</a>
+                 </div>
+            </article>
+            <span className='design'></span>
         </div>
+       </section>
     );
 }
 
