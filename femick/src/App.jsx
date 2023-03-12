36d01e8c11dbd1143273a217b2cc9799bf4262f1
oluwafemimicheal <oluwafemicu@gmail.com>
2023-03-12 03:54:55 +0100

    master

--- femick/src/App.jsx
+++ femick/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from './component/Navbar/Navbar'
 import Home from './component/Home/Home'
 import Platform from './component/Platform/Platform'
 import Solution from './component/Solution/Solution'
@@ -7,6 +6,7 @@ import Resources from './component/Resources/Resources'
 import About from './component/About/About'
 import Contact from './component/Contact/Contact'
 import Testimony from './component/Testimony/Testimony'
+import Footer from './component/Footer/Footer'
 
 
 
@@ -21,6 +21,7 @@ function App() {
     <About/>
     <Testimony/>
     <Contact/>
+    <Footer/>
    </>
   )
 }
---
+++ femick/src/component/Footer/Footer.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container footer-container">
+                <div className="footer-logo">
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                    <span className='flex'>logo</span>
+                   
+                </div>
+                <div className="footer-nav">
+                    <nav>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </nav>
+                    <nav>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </nav>
+                    <nav>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </nav>
+                    <nav>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </nav>
+                    <nav>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </nav>
+                </div>
+                <div className="footer-end">
+                    <small>Copyright 2023</small>
+                    <span>
+                        <small>All Right Reserved</small>
+                        <small>Privacy Notice</small>
+                        <small>Legal</small>
+                        <small>Site map</small>
+                    </span>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
